98f69fbd467b76deceb11fe85343e2c17f857ba6
Luigi160397 <luigi.difraia.97@gmail.com>
2023-06-16 09:59:28 +0200

    Creata base progetto

---
+++ src/test/java/epicode/u5s3g5/U5S3G5ApplicationTests.java
@@ -0,0 +1,13 @@
+package epicode.u5s3g5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S3G5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
